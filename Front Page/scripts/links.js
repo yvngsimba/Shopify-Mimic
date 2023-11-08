de6e5614f4c56a43d406644ae7c70905187cf70c
yvngsimba <52266231+yvngsimba@users.noreply.github.com>
2023-11-08 00:25:41 -0500

    Fixed shopify story functions, continuing main page

--- Front Page/scripts/links.js
+++ Front Page/scripts/links.js
@@ -5,3 +5,39 @@ startFreeTrial.forEach((button) => {
     window.location.href = "https://accounts.shopify.com/store-create?language=en&locale=en-CA&signup_page=https%3A%2F%2Fwww.shopify.com%2Fca&signup_types%5B%5D=paid_trial_experience&_y=6633a316-3fc5-4014-a03b-dd5fff8301e0&_s=4bdf3b30-2e0d-4ef3-9ddd-cb39d8f80aa2&_p=ee847714-611a-4996-a553-7b6acb1408ee"
   });
 });
+
+// Removing and adding classes for video
+
+const storyButton = document.querySelector('.shopify-story');
+const overlayScreen = document.querySelector('.overlay');
+const video = document.querySelector('.story');
+const closeButton = document.querySelector('.close-button');
+
+function activatedButton() {
+  storyButton.addEventListener('click',()=> {
+    overlayScreen.classList.remove('hidden')
+    video.classList.remove('hidden')
+    video.play();
+  });
+}
+
+overlayScreen.addEventListener('click', ()=> {
+  minimizedScreen();
+  overlayScreen.classList.add('hidden')
+});
+activatedButton();
+function minimizedScreen() {
+  video.style.position = "fixed";
+  video.style.top = "750px";
+  video.style.left = "1300px";
+  video.style.height="200px";
+  video.style.width="354px";
+
+  closeButton.classList.remove('hidden')
+};
+
+closeButton.addEventListener('click', () => {
+  video.remove();
+  closeButton.classList.add('hidden');
+});
+
